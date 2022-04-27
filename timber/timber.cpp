6dd1baa05aa3d1ec1f93bf42578f369f7defdfdb
Anurag Sharma <b219008@iiit-bh.ac.in>
2022-04-27 14:24:07 +0530

    moving branches function

--- timber/timber.cpp
+++ timber/timber.cpp
@@ -4,6 +4,40 @@
 
 using namespace sf;
 
+// Function declaration
+void updateBranches(int seed);
+const int NUM_BRANCHES = 6;
+Sprite branches[NUM_BRANCHES];
+
+// Where is the player/branch?
+// Left or Right
+enum class side { LEFT, RIGHT, NONE };
+side branchPositions[NUM_BRANCHES];
+
+// Function definition
+void updateBranches(int seed)
+{
+	// Move all the branches down one place
+	for (int j = NUM_BRANCHES - 1; j > 0; j--) {
+		branchPositions[j] = branchPositions[j - 1];
+	}
+	// Spawn a new branch at position 0
+ // LEFT, RIGHT or NONE
+	srand((int)time(0) + seed);
+	int r = (rand() % 5);
+	switch (r) {
+	case 0:
+		branchPositions[0] = side::LEFT;
+		break;
+	case 1:
+		branchPositions[0] = side::RIGHT;
+		break;
+	default:
+		branchPositions[0] = side::NONE;
+		break;
+	}
+}
+
 int main()
 {
 	//video mode object
@@ -40,19 +74,19 @@ int main()
 	Sprite spriteBee;
 	spriteBee.setTexture(textureBee);
 	spriteBee.setPosition(0, 800);
-	
+
 	// Is the bee currently moving?
 	bool beeActive = false;
-	
+
 	// How fast can the bee fly
 	float beeSpeed = 0.0f;
 
 	// make 3 cloud sprites from 1 texture
 	Texture textureCloud;
-	
+
 	// Load 1 new texture
 	textureCloud.loadFromFile("graphics/cloud.png");
-	
+
 	// 3 New sprites with the same texture
 	Sprite spriteCloud1;
 	Sprite spriteCloud2;
@@ -60,18 +94,18 @@ int main()
 	spriteCloud1.setTexture(textureCloud);
 	spriteCloud2.setTexture(textureCloud);
 	spriteCloud3.setTexture(textureCloud);
-	
+
 	// Position the clouds on the left of the screen
 	// at different heights
 	spriteCloud1.setPosition(0, 0);
 	spriteCloud2.setPosition(0, 250);
 	spriteCloud3.setPosition(0, 500);
-	
+
 	// Are the clouds currently on screen?
 	bool cloud1Active = false;
 	bool cloud2Active = false;
 	bool cloud3Active = false;
-	
+
 	// How fast is each cloud?
 	float cloud1Speed = 0.0f;
 	float cloud2Speed = 0.0f;
@@ -102,19 +136,19 @@ int main()
 	// We need to choose a font
 	Font font;
 	font.loadFromFile("fonts/KOMIKAP_.ttf");
-	
+
 	// Set the font to our message
 	messageText.setFont(font);
 	scoreText.setFont(font);
-	
+
 	// Assign the actual message
 	messageText.setString("Press Enter to start!");
 	scoreText.setString("Score = 0");
-	
+
 	// Make it really big
 	messageText.setCharacterSize(75);
 	scoreText.setCharacterSize(100);
-	
+
 	// Choose a color
 	messageText.setFillColor(Color::White);
 	scoreText.setFillColor(Color::White);
@@ -128,12 +162,31 @@ int main()
 	messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
 	scoreText.setPosition(20, 20);
 
+	// Prepare 6 branches
+	Texture textureBranch;
+	textureBranch.loadFromFile("graphics/branch.png");
+
+	// Set the texture for each branch sprite
+	for (int i = 0; i < NUM_BRANCHES; i++) {
+		branches[i].setTexture(textureBranch);
+		branches[i].setPosition(-2000, -2000);
+		// Set the sprite's origin to dead centre
+        // We can then spin it round without changing its position
+		branches[i].setOrigin(220, 20);
+	}
+
+	updateBranches(1);
+	updateBranches(2);
+	updateBranches(3);
+	updateBranches(4);
+	updateBranches(5);
+
 	while (window.isOpen())
 	{
 		/**********handle the player input**********/
-		
+
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
-		
+
 		{
 			window.close();
 		}
@@ -148,7 +201,7 @@ int main()
 			timeRemaining = 12;
 		}
 
-		
+
 		/**********update the scene**********/
 
 		if (!paused)
@@ -290,26 +343,57 @@ int main()
 			std::stringstream ss;
 			ss << "Score = " << score;
 			scoreText.setString(ss.str());
+
+			// update the branch sprites
+			for (int i = 0; i < NUM_BRANCHES; i++)
+			{
+				float height = i * 150;
+				if (branchPositions[i] == side::LEFT)
+				{
+					// Move the sprite to the left side
+					branches[i].setPosition(610, height);
+					// Flip the sprite round the other way
+					branches[i].setRotation(180);
+				}
+				else if (branchPositions[i] == side::RIGHT)
+				{
+					// Move the sprite to the right side
+					branches[i].setPosition(1330, height);
+					// Set the sprite rotation to normal
+					branches[i].setRotation(0);
+				}
+				else
+				{
+					// Hide the branch
+					branches[i].setPosition(3000, height);
+				}
+			}
+
 		}//END if (!paused)
 
 		/**********draw the scene**********/
-		
+
 		//clear everything from last frame
-		
+
 		window.clear();
-		
+
 		//draw our game scene here
-		
+
 		window.draw(spritebackground);
 
 		// Draw the clouds
 		window.draw(spriteCloud1);
 		window.draw(spriteCloud2);
 		window.draw(spriteCloud3);
-		
+
+		// Draw the branches
+		for (int i = 0; i < NUM_BRANCHES; i++) {
+			window.draw(branches[i]);
+		}
+
 		// Draw the tree
 		window.draw(spriteTree);
-		
+
 		// Draw the insect
 		window.draw(spriteBee);
 
@@ -324,7 +408,7 @@ int main()
 			// Draw our message
 			window.draw(messageText);
 		}
-		
+
 		//show everything we just draw
 		window.display();
 	}
